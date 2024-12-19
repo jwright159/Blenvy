1a508b1a0ff986d3970bb5842fa382a2f539ae32
Jady Wright <iamjwright159@gmail.com>
2024-12-18 20:21:35 -0500

    Fix Vec serialization

--- tools/blenvy/add_ons/bevy_components/propGroups/conversions_from_prop_group.py
+++ tools/blenvy/add_ons/bevy_components/propGroups/conversions_from_prop_group.py
@@ -8,19 +8,19 @@ conversion_tables = {
     "alloc::string::String": lambda value: '"'+str(value)+'"',
     "alloc::borrow::Cow<str>": lambda value: '"'+str(value)+'"',
 
-    "glam::Vec2": lambda value: "Vec2(x:"+str(value[0])+ ", y:"+str(value[1])+")",
-    "glam::DVec2": lambda value: "DVec2(x:"+str(value[0])+ ", y:"+str(value[1])+")",
-    "glam::UVec2": lambda value: "UVec2(x:"+str(int(value[0]))+ ", y:"+str(int(value[1]))+")",
+    "glam::Vec2": lambda value: "("+str(value[0])+ ", "+str(value[1])+")",
+    "glam::DVec2": lambda value: "("+str(value[0])+ ", "+str(value[1])+")",
+    "glam::UVec2": lambda value: "("+str(value[0])+ ", "+str(value[1])+")",
 
-    "glam::Vec3": lambda value: "Vec3(x:"+str(value[0])+ ", y:"+str(value[1])+ ", z:"+str(value[2])+")",
-    "glam::Vec3A": lambda value: "Vec3A(x:"+str(value[0])+ ", y:"+str(value[1])+ ", z:"+str(value[2])+")",
-    "glam::UVec3": lambda value: "UVec3(x:"+str(int(value[0]))+ ", y:"+str(int(value[1]))+ ", z:"+str(int(value[2]))+")",
+    "glam::Vec3": lambda value: "("+str(value[0])+ ", "+str(value[1])+ ", "+str(value[2])+")",
+    "glam::Vec3A": lambda value: "("+str(value[0])+ ", "+str(value[1])+ ", "+str(value[2])+")",
+    "glam::UVec3": lambda value: "("+str(value[0])+ ", "+str(value[1])+ ", "+str(value[2])+")",
 
-    "glam::Vec4": lambda value: "Vec4(x:"+str(value[0])+ ", y:"+str(value[1])+ ", z:"+str(value[2])+ ", w:"+str(value[3])+")",
-    "glam::DVec4": lambda value: "DVec4(x:"+str(value[0])+ ", y:"+str(value[1])+ ", z:"+str(value[2])+ ", w:"+str(value[3])+")",
-    "glam::UVec4": lambda value: "UVec4(x:"+str(int(value[0]))+ ", y:"+str(int(value[1]))+ ", z:"+str(int(value[2]))+ ", w:"+str(int(value[3]))+")",
+    "glam::Vec4": lambda value: "("+str(value[0])+ ", "+str(value[1])+ ", "+str(value[2])+ ", "+str(value[3])+")",
+    "glam::DVec4": lambda value: "("+str(value[0])+ ", "+str(value[1])+ ", "+str(value[2])+ ", "+str(value[3])+")",
+    "glam::UVec4": lambda value: "("+str(value[0])+ ", "+str(value[1])+ ", "+str(value[2])+ ", "+str(value[3])+")",
 
-    "glam::Quat":  lambda value: "Quat(x:"+str(value[0])+ ", y:"+str(value[1])+ ", z:"+str(value[2])+ ", w:"+str(value[3])+")",
+    "glam::Quat":  lambda value: "("+str(value[0])+ ", "+str(value[1])+ ", "+str(value[2])+ ", "+str(value[3])+")",
 
     "bevy_color::srgba::Srgba":  lambda value: "Srgba(red:"+str(value[0])+ ", green:"+str(value[1])+ ", blue:"+str(value[2])+ ", alpha:"+str(value[3])+   ")",
     "bevy_color::linear_rgba::LinearRgba":  lambda value: "LinearRgba(red:"+str(value[0])+ ", green:"+str(value[1])+ ", blue:"+str(value[2])+ ", alpha:"+str(value[3])+   ")",
